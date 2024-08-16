f9e04ad37c07f5e24a34f0344c4d4a60d4f84cce
Matias <mati.adagio@gmail.com>
2024-08-15 22:42:33 -0300

    define workflow

--- main.java
+++ main.java
@@ -1,17 +1,12 @@
 public class Suma {
     public static void main(String[] args) {
-        // Declaración de variables
         int numero1 = 5;
         int numero2 = 10;
 
-        // Llamada a la función que suma los dos números
         int resultado = sumar(numero1, numero2);
 
-        // Imprimir el resultado en la consola
         System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
     }
-
-    // Método que suma dos números enteros
     public static int sumar(int a, int b) {
         return a + b;
     }
