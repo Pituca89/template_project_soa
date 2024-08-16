1585d52cd02761c629e4ad5a61b719f063545f5c
Matias <mati.adagio@gmail.com>
2024-08-16 00:18:00 -0300

    define workflow

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,25 @@
-public class Calculator {
+public class Calculator 
+{
 
-    public int add(int a, int b) {
+    public int add(int a, int b) 
+    {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    public int subtract(int a, int b) 
+    {
         return a - b;
     }
 
-    public int multiply(int a, int b) {
+    public int multiply(int a, int b) 
+    {
         return a * b;
     }
 
-    public int divide(int a, int b) throws IllegalArgumentException {
-        if (b == 0) {
+    public int divide(int a, int b) throws IllegalArgumentException 
+    {
+        if (b == 0) 
+        {
             throw new IllegalArgumentException("Division by zero is not allowed.");
         }
         return a / b;
